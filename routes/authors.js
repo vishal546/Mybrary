d5f8618ca98e7a11797886526f64a459908cfc50
vishal <vishalsharma.vs546@gmail.com>
2021-08-03 19:26:22 +0530

    finish author routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const router=express.Router()
 const Author=require("../models/author")
+const Book=require("../models/book")
 
 
 //All authors route
@@ -36,8 +37,8 @@ router.post("/",async (req,res)=>{
 
 try{
 const newAuthor=await author.save()
-    // res.redirect("authors/${newAuthor.id}");
-res.redirect("authors")
+     res.redirect(`authors/${newAuthor.id}`);
+//res.redirect("authors")
 }catch {
     res.render("authors/new",{
         author:author,
@@ -46,7 +47,101 @@ res.redirect("authors")
 }
 
     })
+
+
+    router.get("/:id",async (req,res)=>{
+
+        try{
+
+const author=await Author.findById(req.params.id)
+const books=await Book.find({author:author.id}).limit(6).exec()
+res.render('authors/show',{
+    author:author,
+    booksByAuthor:books
+})
+        
+}catch{
+           
+            res.redirect("/")
+        }
+
+
+      
+    })
     
+
+    router.get("/:id/edit",async (req,res)=>{
+
+
+        try{
+            const author=await Author.findById(req.params.id)
+
+            res.render("authors/edit",{author:author});
+
+        }catch{
+            res.redirect("/authors")
+        }
+
+       
+        res.send("Edit Author "+req.params.id)
+    })
+
+  
+  
+    router.put("/:id",async (req,res)=>{
+
+    let author
+    
+    try{
+        author=await Author.findById(req.params.id)
+        author.name=req.body.name
+        await author.save()
+        // res.redirect("authors/${newAuthor.id}");
+    res.redirect(`/authors/${author.id}`)
+    }catch {
+
+        if(author==null)
+        {
+res.redirect("/")
+        }else{
+            res.render("authors/edit",{
+                author:author,
+                errorMessage:"Error updating Author"
+            })
+        }
+
+        
+    }
+
+
+       // res.send("Update Author "+req.params.id)
+    })
+
+    router.delete("/:id",async (req,res)=>{
+        let author
+        try{
+            author=await Author.findById(req.params.id)
+          
+            await author.remove()
+        res.redirect("/authors")
+        }catch {
+    
+            if(author==null)
+            {
+    res.redirect("/")
+            }else{
+                res.redirect(`/authors/${author.id}`)
+            }
+    
+            
+        }
+
+
+       // res.send("Delete Author "+req.params.id)
+    })
+
+
+   
 
 
 
